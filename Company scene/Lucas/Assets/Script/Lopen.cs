4fa5fa3726636ac0067ba8e426de5f9050cea8b9
JesperKiers <jeppywho@gmail.com>
2017-01-27 21:20:30 +0100

    +company

---
+++ Company scene/Lucas/Assets/Script/Lopen.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lopen : MonoBehaviour {
+
+    public Vector3 v3;
+    public float hor;
+    public float ver;
+    public float speed;
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        hor = Input.GetAxis("Horizontal");
+        ver = Input.GetAxis("Vertical");
+        v3.z = ver;
+        v3.x = hor;
+        
+
+        transform.Translate(v3*Time.deltaTime*speed);
+	
+	}
+}
